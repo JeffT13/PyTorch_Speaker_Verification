cb6575d5841a58214b2a0166cb2aebb90963a12a
JeffT13 <jefftumminia@gmail.com>
2020-11-18 13:08:23 -0500

    fixed bash script

--- princerun.sh
+++ princerun.sh
@@ -6,9 +6,10 @@
 #SBATCH --mem=5GB
 #SBATCH --job-name=dvec_SCOTUS
 
-module purge
-module load anaconda3/5.3.1
-module load cuda/10.0.130
-module load gcc/6.3.0
-cd ~/PyTorch_Speaker_Verification
+##module purge
+##module load anaconda3/5.3.1
+##module load cuda/10.0.130
+##module load gcc/6.3.0
+##source activate /scratch/jt2565/capston/newenv
+##cd ~/PyTorch_Speaker_Verification
 python dvector_create_SCOTUS.py
