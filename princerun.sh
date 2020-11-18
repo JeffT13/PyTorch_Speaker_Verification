8ab2d2ec591398c02b63ed3b51524d82bfc8eb2b
JeffT13 <jefftumminia@gmail.com>
2020-11-18 17:00:32 -0500

    git test

--- princerun.sh
+++ princerun.sh
@@ -9,16 +9,3 @@
 #SBATCH --output=scotusprint.out
 
 python dvector_SCOTUS.py
-=======
-#!/bin/bash -e
-#
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
-#SBATCH --time=4:00:00
-#SBATCH --mem=16GB
-#SBATCH --job-name=dvec_SCOTUS
-#SBATCH --output=scotusprint.out
-
-python dvector_create_SCOTUS.py
->>>>>>> master
