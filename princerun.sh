05212f9b6479c411816cece892817da78a7af3f6
JeffT13 <jefftumminia@gmail.com>
2020-11-18 12:43:36 -0500

    update prince script

--- princerun.sh
+++ princerun.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
-
+#!/bin/bash -e
+#
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=4:00:00
-#SBATCH --mem=16GB
+#SBATCH --mem=5GB
+#SBATCH --job-name=dvec_SCOTUS
 
 module purge
 module load anaconda3/5.3.1
 module load cuda/10.0.130
 module load gcc/6.3.0
-NETID=jt2565
-source activate /scratch/${NETID}/capstone/newenv
+cd ~/PyTorch_Speaker_Verification
 python dvector_create_SCOTUS.py
