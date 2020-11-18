64e607f8eac5bf7dff1eef9c0030e2efae748ca6
JeffT13 <jefftumminia@gmail.com>
2020-11-18 17:46:52 -0500

    sergey meeting

--- princerun.sh
+++ princerun.sh
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=4:00:00
+#SBATCH --gres=gpu:1
 #SBATCH --mem=16GB
 #SBATCH --job-name=dvec_SCOTUS
-#SBATCH --output=scotusprint.out
+##SBATCH --output=scotusprint.out
 
 python dvector_SCOTUS.py
