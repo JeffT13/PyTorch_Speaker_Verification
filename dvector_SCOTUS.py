7d10ccf068af0498833cf418e12f3d25312e1e8f
JeffT13 <jefftumminia@gmail.com>
2020-11-20 21:14:40 -0500

    formatting

--- dvector_SCOTUS.py
+++ dvector_SCOTUS.py
@@ -121,99 +121,99 @@ Saves
 '''
 
 for i, folder in enumerate(case_path):
-  case = folder.split('/')[-1]
-  if verbose:
-    print("Processing case:", case)
-
-  case_file_lst = []
-  case_sequence = []
-  case_cluster_id = []
-  spkrtracker=0
-  
-  for spkr_name in os.listdir(folder):
-    casecount = 0
+    case = folder.split('/')[-1]
     if verbose:
-      print("Processing spkr:", spkr_name)
+        print("Processing case:", case)
 
-    #Build Judge Dictionary & Handle Other
-    if spkr_name[-14:]=='scotus_justice':
-      if spkr_name[:-15] in judge_dict:
-        use_label = judge_dict[spkr_name[:-15]]
-      else:
-        judge_dict[spkr_name[:-15]] = cnt
-        use_label = judge_dict[spkr_name[:-15]]
-        cnt+=1
-    else:
-      use_label = label 
-      label+=1
+    case_file_lst = []
+    case_sequence = []
+    case_cluster_id = []
+    spkrtracker=0
 
-    spkr_file_lst = []
-    spkr_sequence = []
-    spkr_cluster_lst = []
+    for spkr_name in os.listdir(folder):
+        casecount = 0
+        if verbose:
+            print("Processing spkr:", spkr_name)
 
-    for file in os.listdir(folder+'/'+spkr_name):
-      if file[-4:] == '.wav':
-        times, segs = VAD_chunk(2, folder+'/'+spkr_name+'/'+file)
+        #Build Judge Dictionary & Handle Other
+        if spkr_name[-14:]=='scotus_justice':
+            if spkr_name[:-15] in judge_dict:
+                use_label = judge_dict[spkr_name[:-15]]
+            else:
+                judge_dict[spkr_name[:-15]] = cnt
+                use_label = judge_dict[spkr_name[:-15]]
+                cnt+=1
+        else:
+            use_label = label 
+            label+=1
 
-        # Bad .wav detection
-        if segs == []:
-          #print('No voice activity detected')
-          rm_pthlst.append(folder+'/'+file)
-          continue
+        spkr_file_lst = []
+        spkr_sequence = []
+        spkr_cluster_lst = []
 
-        concat_seg = concat_segs(times, segs)
-        if len(concat_seg)<min_va:
-          #print('Below Minimum voice activity detected')
-          rm_pthlst.append(folder+'/'+file)
-          continue
-           
-        STFT_frames = get_STFTs(concat_seg)
-        STFT_frames = np.stack(STFT_frames, axis=2)
-        STFT_frames = torch.tensor(np.transpose(STFT_frames, axes=(2,1,0)))
-        STFT_frames = STFT_frames.to(hp.device)
-        embeddings = embedder_net(STFT_frames)
-        aligned_embeddings = align_embeddings(embeddings.detach().cpu().numpy())
-        
-        spkr_sequence.append(aligned_embeddings)
-        spkr_cluster_id = []
-        for embedding in aligned_embeddings:
-          spkr_cluster_id.append(str(use_label)) #use_label handling judge id
+        for file in os.listdir(folder+'/'+spkr_name):
+            if file[-4:] == '.wav':
+                times, segs = VAD_chunk(2, folder+'/'+spkr_name+'/'+file)
 
-        #Track full names of processed wav files
-        pth = file.split(".")[0]+'.txt'
-        pth = case+'/'+spkr_name+'/'+pth
-        f = open(hp.data.main_path+pth, 'r')
-        f = f.read().split(" ")
-        spkr_file_lst.append((f[0], f[1], np.shape(aligned_embeddings)[0], casecount, spkrtracker))
-        spkr_cluster_lst.append(spkr_cluster_id)
-        casecount = casecount + 1
+                # Bad .wav detection
+                if segs == []:
+                    #print('No voice activity detected')
+                    rm_pthlst.append(folder+'/'+file)
+                    continue
+
+                concat_seg = concat_segs(times, segs)
+                if len(concat_seg)<min_va:
+                    #print('Below Minimum voice activity detected')
+                    rm_pthlst.append(folder+'/'+file)
+                    continue
+
+                STFT_frames = get_STFTs(concat_seg)
+                STFT_frames = np.stack(STFT_frames, axis=2)
+                STFT_frames = torch.tensor(np.transpose(STFT_frames, axes=(2,1,0)))
+                STFT_frames = STFT_frames.to(hp.device)
+                embeddings = embedder_net(STFT_frames)
+                aligned_embeddings = align_embeddings(embeddings.detach().cpu().numpy())
+
+                spkr_sequence.append(aligned_embeddings)
+                spkr_cluster_id = []
+                for embedding in aligned_embeddings:
+                    spkr_cluster_id.append(str(use_label)) #use_label handling judge id
+
+                #Track full names of processed wav files
+                pth = file.split(".")[0]+'.txt'
+                pth = case+'/'+spkr_name+'/'+pth
+                f = open(hp.data.main_path+pth, 'r')
+                f = f.read().split(" ")
+                spkr_file_lst.append((f[0], f[1], np.shape(aligned_embeddings)[0], casecount, spkrtracker))
+                spkr_cluster_lst.append(spkr_cluster_id)
+                casecount = casecount + 1
+
+        if verbose:
+        print('Processed', casecount, 'files for case', case, 'for spkr', spkr_name)
+        case_file_lst.append(spkr_file_lst)
+        case_sequence.append(spkr_sequence)
+        case_cluster_id.append(spkr_cluster_lst)
+        spkrtracker+=1
 
     if verbose:
-      print('Processed', casecount, 'files for case', case, 'for spkr', spkr_name)
-    case_file_lst.append(spkr_file_lst)
-    case_sequence.append(spkr_sequence)
-    case_cluster_id.append(spkr_cluster_lst)
-    spkrtracker+=1
-
-  if verbose:
     print('Handled', spkrtracker, 'speakers for case', case)
     print('saving case sequence', case)
 
-  fold = hp.data.save_path+case+'/'
-  if not os.path.exists(fold):
+    fold = hp.data.save_path+case+'/'
+    if not os.path.exists(fold):
     os.makedirs(fold)
-  temp_sequence = np.asarray(case_sequence, dtype='object')
-  temp_cluster_id = np.asarray(case_cluster_id, dtype='object')
-  np.save(fold+case+'_embarr',temp_sequence)
-  np.save(fold+case+'_labelarr',temp_cluster_id)
-  temp_sequence = []
-  temp_cluster_id = []
+    temp_sequence = np.asarray(case_sequence, dtype='object')
+    temp_cluster_id = np.asarray(case_cluster_id, dtype='object')
+    np.save(fold+case+'_embarr',temp_sequence)
+    np.save(fold+case+'_labelarr',temp_cluster_id)
+    temp_sequence = []
+    temp_cluster_id = []
 
-  info_lst=[item for sublist in case_file_lst for item in sublist]
-  with open(fold+case+'_info.csv', 'w+') as file:     
+    info_lst=[item for sublist in case_file_lst for item in sublist]
+    with open(fold+case+'_info.csv', 'w+') as file:     
     write = csv.writer(file) 
     write.writerows(info_lst)
 
-  with open(fold+case+'_2remove.csv', 'w') as rm:
+    with open(fold+case+'_2remove.csv', 'w') as rm:
     wr = csv.writer(rm, delimiter=",")
     wr.writerow(rm_pthlst)
