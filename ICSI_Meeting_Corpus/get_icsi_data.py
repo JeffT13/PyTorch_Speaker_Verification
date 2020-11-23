cd24c244ea86a6648dbd8197b9cac8a3c146296a
Amanda Kuznecov <54920249+akuz91@users.noreply.github.com>
2020-11-22 23:29:51 -0500

    added script for downloading ICSI Meeting Corpus

---
+++ ICSI_Meeting_Corpus/get_icsi_data.py
@@ -0,0 +1,22 @@
+import os
+
+'''This script downloads audio and transcript files from the ICSI Meeting Corpus.'''
+
+path = './icsi_data'
+
+#create new directories to store raw files
+os.makedirs(path+'/raw_transcripts')
+os.makedirs(path+'/raw_audio')
+
+#download raw transcripts
+!wget -P ./icsi_data/raw_transcripts http://groups.inf.ed.ac.uk/ami/ICSICorpusAnnotations/ICSI_original_transcripts.zip
+
+import zipfile
+with zipfile.ZipFile(path+'/raw_transcripts/ICSI_original_transcripts.zip','r') as zip_ref:
+    zip_ref.extractall('./icsi_data/raw_transcripts/')
+
+#download raw audio files
+files = os.listdir(path+'/raw_transcripts/transcripts')
+for i in files:
+    i=i.strip('.mrt')
+    os.system('wget -P ./icsi_data/raw_audio http://groups.inf.ed.ac.uk/ami//ICSIsignals/NXT/%s.interaction.wav'%i)
